79b8b9bcab1ff155da30698e97ff8b552f670d6b
Priya-Sivalingam <priyasivalingam99@gmail.com>
2024-09-27 22:29:14 +0530

    Initial commit

---
+++ src/main/java/com/employeemanagementsys/employeemanagementsys_api/EmployeemanagementsysApiApplication.java
@@ -0,0 +1,13 @@
+package com.employeemanagementsys.employeemanagementsys_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagementsysApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagementsysApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employeemanagementsys/employeemanagementsys_api/EmployeemanagementsysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employeemanagementsys.employeemanagementsys_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagementsysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
