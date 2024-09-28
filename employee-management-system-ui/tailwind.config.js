2c2df1215997a6bfe028a8e847c6d759382dc034
Priya-Sivalingam <priyasivalingam99@gmail.com>
2024-09-28 05:30:58 +0530

    add files

--- employee-management-system-ui/tailwind.config.js
+++ employee-management-system-ui/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust according to your file structure
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
